13ed73c20a5f98153b2ff5a333dd3dea4d5947a5
Ben Edwards <hello@benedfit.com>
2020-08-21 09:51:45 +0100

    feat(header): simplifies file structure

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,13 +3,18 @@ import Link from 'next/link'
 import { ContentContainer, Icon, SmartLink } from '@newhighsco/chipset'
 import { config } from '../../../site.config'
 
-import theme from './theme.module.scss'
-import styles from './styles.module.scss'
+import styles from './Header.module.scss'
 import LogoSvg from '../../images/logo-full.svg'
 
 const Header = () => (
   <>
-    <ContentContainer id="top" as="header" role="banner" gutter theme={theme}>
+    <ContentContainer
+      id="top"
+      as="header"
+      role="banner"
+      gutter
+      theme={{ content: styles.content, gutter: styles.gutter }}
+    >
       <Link href="/" passHref>
         <SmartLink>
           <Icon alt={config.name} theme={{ wrapper: styles.logo }}>
